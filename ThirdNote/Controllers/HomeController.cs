7e6f596e3eff03fbbc3ca8055d5a9142b669986e
sina33 <sina.saeedi.edu@gmail.com>
2022-01-31 16:21:17 +0330

    move ironwill to Home

--- ThirdNote/Controllers/HomeController.cs
+++ ThirdNote/Controllers/HomeController.cs
@@ -3,14 +3,46 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using ThirdNote.ViewModels;
+using ThirdNote.Models;
 
 namespace ThirdNote.Controllers
 {
     public class HomeController : Controller
     {
+
+        private NotebookDbContext db = new NotebookDbContext();
+
         public ActionResult Index()
         {
-            return View();
+            Dashboard indexViewData = new Dashboard()
+            {
+                Pinned = db.Notes.Where(n => n.Pin).ToList(),
+                Sorted = db.Notes.OrderByDescending(n => n.WrittenDate).ThenByDescending(n => n.Id).ToList()
+            };
+            if (db.Relapses.Count() > 0)
+            {
+                var dates = db.Relapses.OrderByDescending(r => r.Date).Select(r => r.Date).ToList();
+                var recentRelapseDate = DateTime.Now;
+                List<int> Strikes = new List<int>();
+                foreach (var item in dates)
+                {
+                    int offset = (recentRelapseDate - item).Days;
+                    Strikes.Add(offset);
+                    recentRelapseDate = item;
+                }
+                //Strikes.Reverse();
+                indexViewData.Strikes = Strikes;
+                //indexViewData.LastStrike = (DateTime.Now - db.Relapses.OrderByDescending(r => r.Date).First().Date).Days;
+                System.Diagnostics.Debug.WriteLine(dates);
+                System.Diagnostics.Debug.WriteLine(Strikes);
+            }
+            else
+            {
+                indexViewData.Strikes = new List<int> { 0 };
+            }
+            //return View(db.Notes.OrderByDescending(n => n.WrittenDate).ThenByDescending(n => n.Id).ToList());
+            return View(indexViewData);
         }
 
         public ActionResult About()
