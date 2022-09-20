3ddfba4319dc725eaff3896138e1a5d88e451b67
sina33 <sina.saeedi.edu@gmail.com>
2022-09-20 23:10:18 +0430

    table headers become clickable to sort column

--- ThirdNote/ViewModels/Home.cs
+++ ThirdNote/ViewModels/Home.cs
@@ -6,9 +6,9 @@ using ThirdNote.Models;
 
 namespace ThirdNote.ViewModels
 {
-    public class Dashboard
+    public class Home
     {
-        public IEnumerable<Note> Pinned { get; set; }
+        public IEnumerable<Note> Private { get; set; }
         public IEnumerable<Note> Sorted { get; set; }
         public List<int> Strikes { get; set; }
     }
