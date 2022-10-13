daa4bd569811ad9f447d226fbf393f48a5a97057
sina33 <sina.saeedi.edu@gmail.com>
2022-10-14 00:53:36 +0430

    note edit view is modified according to note create view

--- ThirdNote/Scripts/dir-auto.js
+++ ThirdNote/Scripts/dir-auto.js
@@ -4,7 +4,7 @@ function set_val_dir() {
 		var arabic = /[\u0600-\u06FF]/g; //setting arabic unicode
 		var isRTL = $(this).val().charAt(0).match(arabic);
 		console.log($(this).val())
-		if (isRTL) {
+		if (isRTL || $(this).val().length==0) {
 			//$(this).attr('direction', 'rtl');
 			//$(this).parent().attr('direction', 'rtl');
 			$(this).addClass('dir-rtl').removeClass('dir-ltr');
@@ -23,7 +23,7 @@ function set_txt_dir() {
 		var arabic = /[\u0600-\u06FF]/g; //setting arabic unicode
 		var isRTL = $(this).text().trim().charAt(0).match(arabic);
 		console.log("text:", $(this).text().trim().charAt(0))
-		if (isRTL) {
+		if (isRTL || $(this).text().length == 0) {
 			//$(this).attr('direction', 'rtl');
 			//$(this).parent().attr('direction', 'rtl');
 			$(this).addClass('dir-rtl').removeClass('dir-ltr');
